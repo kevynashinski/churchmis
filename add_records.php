8c5abe23c80151d583bb52995b04a7d54fc10b56
kevynashinski <kevinwafula60@gmail.com>
2016-09-21 14:02:56 +0300

    kevynashinski

--- add_records.php
+++ add_records.php
@@ -78,7 +78,6 @@ if(isset($_GET['inserted'])){
     <div class="row">
         <div class="col-md-12 col-lg-12 col-sm-12">
             <h1 style="text-align:center; font-size:4em;">Welcome to churchMis</h1>
-            <h2 style="text-align:center;">an online place for our congregants</h2>
             <h3 style="text-align:center">Enter Record</h3>
         </div>
     </div>
