bc5246214dbd95cf5cfd3ba648ec2580f3ec38fd
kevynashinski <kevinwafula60@gmail.com>
2016-09-22 00:43:20 +0300

    freddie badman

---
+++ churchnews.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevyn-yilmaz
+ * Date: 8/22/16
+ * Time: 11:46 AM
+ */
+
+include_once 'includes/config.php';
+//include_once 'includes/session.php';
+?>
+    <html lang="en-US">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="classwork, follwoing, chatting, liking">
+        <title>churchmis</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+
+
+        <script type="application/javascript">
+
+
+        </script>
+
+    </head>
+    <body class="bgmain">
+
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                        aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+            </div>
+            <div id="navbar" class="navbar-collapse collapse navbar-right">
+                <ul class="nav navbar-nav"></ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-lg-12 col-sm-12">
+                <h1 style="text-align:center; font-size:4em;">church fundraising</h1>
+                <h2 style="text-align:center;">concerns the fundraising to the needy children course: </h2>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="container">
+        <div class="dataView">
+            <h2>date:to be held on 12 nov 2016</h2>
+            <h2>venue:the church mainhall</h2>
+            <h2>we wish to invite all members to a church fundraiser.come all so as to support the needy in our
+                community</h2>
+        </div>
+    </div>
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+        </div>
+        <div id="navbar" class="navbar-collapse collapse navbar-right">
+            <ul class="nav navbar-nav"></ul>
+        </div><!--/.nav-collapse -->
+    </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-lg-12 col-sm-12">
+                <h1 style="text-align:center; font-size:4em;">church youth</h1>
+                <h2 style="text-align:center;">concerning trip: </h2>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="container">
+        <div class="dataView">
+            <h2>date: 10 october 2016</h2>
+            <h2>we wish to communicate about the trip to nakuru which will involve conducting of community welfare in
+                needy villages.</h2>
+            <h2>all youths who wish to attend should lias with their youth leaders</h2>
+        </div>
+    </div>
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+        </div>
+        <div id="navbar" class="navbar-collapse collapse navbar-right">
+            <ul class="nav navbar-nav"></ul>
+        </div><!--/.nav-collapse -->
+    </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-lg-12 col-sm-12">
+                <h1 style="text-align:center; font-size:4em;">Contacts</h1>
+                <h2 style="text-align:center;">You can easily reach us via: </h2>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="container">
+        <div class="dataView">
+            <h2>Email: support@church.info</h2>
+            <h2>Phone: +254716 567234</h2>
+            <h2>Mobile: +254723 334678</h2>
+        </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    </body>
+    </html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: freddie
+ * Date: 9/22/2016
+ * Time: 12:25 AM
+ */
